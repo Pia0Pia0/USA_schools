b38b5589bff21b5218cdd436f7ad859f0b52a582
whoisescitalopram <36186058+whoisescitalopram@users.noreply.github.com>
2018-03-22 00:14:50 +1100

    Add Schools.py

---
+++ Schools.py
@@ -0,0 +1,284 @@
+
+# coding: utf-8
+
+# # Read in the data
+
+# In[2]:
+
+
+import pandas
+import numpy
+import re
+
+data_files = [
+    "ap_2010.csv",
+    "class_size.csv",
+    "demographics.csv",
+    "graduation.csv",
+    "hs_directory.csv",
+    "sat_results.csv"
+]
+
+data = {}
+
+for f in data_files:
+    d = pandas.read_csv("schools/{0}".format(f))
+    data[f.replace(".csv", "")] = d
+
+
+# # Read in the surveys
+
+# In[3]:
+
+
+all_survey = pandas.read_csv("schools/survey_all.txt", delimiter="\t", encoding='windows-1252')
+d75_survey = pandas.read_csv("schools/survey_d75.txt", delimiter="\t", encoding='windows-1252')
+survey = pandas.concat([all_survey, d75_survey], axis=0)
+
+survey["DBN"] = survey["dbn"]
+
+survey_fields = [
+    "DBN", 
+    "rr_s", 
+    "rr_t", 
+    "rr_p", 
+    "N_s", 
+    "N_t", 
+    "N_p", 
+    "saf_p_11", 
+    "com_p_11", 
+    "eng_p_11", 
+    "aca_p_11", 
+    "saf_t_11", 
+    "com_t_11", 
+    "eng_t_10", 
+    "aca_t_11", 
+    "saf_s_11", 
+    "com_s_11", 
+    "eng_s_11", 
+    "aca_s_11", 
+    "saf_tot_11", 
+    "com_tot_11", 
+    "eng_tot_11", 
+    "aca_tot_11",
+]
+survey = survey.loc[:,survey_fields]
+data["survey"] = survey
+
+
+# # Add DBN columns
+
+# In[4]:
+
+
+data["hs_directory"]["DBN"] = data["hs_directory"]["dbn"]
+
+def pad_csd(num):
+    string_representation = str(num)
+    if len(string_representation) > 1:
+        return string_representation
+    else:
+        return "0" + string_representation
+    
+data["class_size"]["padded_csd"] = data["class_size"]["CSD"].apply(pad_csd)
+data["class_size"]["DBN"] = data["class_size"]["padded_csd"] + data["class_size"]["SCHOOL CODE"]
+
+
+# # Convert columns to numeric
+
+# In[5]:
+
+
+cols = ['SAT Math Avg. Score', 'SAT Critical Reading Avg. Score', 'SAT Writing Avg. Score']
+for c in cols:
+    data["sat_results"][c] = pandas.to_numeric(data["sat_results"][c], errors="coerce")
+
+data['sat_results']['sat_score'] = data['sat_results'][cols[0]] + data['sat_results'][cols[1]] + data['sat_results'][cols[2]]
+
+def find_lat(loc):
+    coords = re.findall("\(.+, .+\)", loc)
+    lat = coords[0].split(",")[0].replace("(", "")
+    return lat
+
+def find_lon(loc):
+    coords = re.findall("\(.+, .+\)", loc)
+    lon = coords[0].split(",")[1].replace(")", "").strip()
+    return lon
+
+data["hs_directory"]["lat"] = data["hs_directory"]["Location 1"].apply(find_lat)
+data["hs_directory"]["lon"] = data["hs_directory"]["Location 1"].apply(find_lon)
+
+data["hs_directory"]["lat"] = pandas.to_numeric(data["hs_directory"]["lat"], errors="coerce")
+data["hs_directory"]["lon"] = pandas.to_numeric(data["hs_directory"]["lon"], errors="coerce")
+
+
+# # Condense datasets
+
+# In[6]:
+
+
+class_size = data["class_size"]
+class_size = class_size[class_size["GRADE "] == "09-12"]
+class_size = class_size[class_size["PROGRAM TYPE"] == "GEN ED"]
+
+class_size = class_size.groupby("DBN").agg(numpy.mean)
+class_size.reset_index(inplace=True)
+data["class_size"] = class_size
+
+data["demographics"] = data["demographics"][data["demographics"]["schoolyear"] == 20112012]
+
+data["graduation"] = data["graduation"][data["graduation"]["Cohort"] == "2006"]
+data["graduation"] = data["graduation"][data["graduation"]["Demographic"] == "Total Cohort"]
+
+
+# # Convert AP scores to numeric
+
+# In[7]:
+
+
+cols = ['AP Test Takers ', 'Total Exams Taken', 'Number of Exams with scores 3 4 or 5']
+
+for col in cols:
+    data["ap_2010"][col] = pandas.to_numeric(data["ap_2010"][col], errors="coerce")
+
+
+# # Combine the datasets
+
+# In[8]:
+
+
+combined = data["sat_results"]
+
+combined = combined.merge(data["ap_2010"], on="DBN", how="left")
+combined = combined.merge(data["graduation"], on="DBN", how="left")
+
+to_merge = ["class_size", "demographics", "survey", "hs_directory"]
+
+for m in to_merge:
+    combined = combined.merge(data[m], on="DBN", how="inner")
+
+combined = combined.fillna(combined.mean())
+combined = combined.fillna(0)
+
+
+# # Add a school district column for mapping
+
+# In[9]:
+
+
+def get_first_two_chars(dbn):
+    return dbn[0:2]
+
+combined["school_dist"] = combined["DBN"].apply(get_first_two_chars)
+
+
+# # Find correlations
+
+# In[10]:
+
+
+correlations = combined.corr()
+correlations = correlations["sat_score"]
+print(correlations)
+
+
+# In[11]:
+
+
+combined.head()
+
+
+# In[12]:
+
+
+combined.corr()["sat_score"][survey_fields].plot.bar()
+
+
+# In[13]:
+
+
+combined.corr()["sat_score"][survey_fields]
+
+
+# In[14]:
+
+
+combined.corr()["sat_score"]
+
+
+# In[15]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+combined.corr()["sat_score"][survey_fields].plot.bar()
+
+
+# In[19]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+plt.scatter(combined['saf_s_11'],combined['sat_score'])
+
+
+# In[20]:
+
+
+races=['white_per','asian_per','black_per','hispanic_per']
+combined.corr()['sat_score'][races].plot.bar()
+
+
+# In[22]:
+
+
+combined.plot.scatter('hispanic_per','sat_score')
+
+
+# Although it shows that schools with less than 30% of hispanic students have relatively higher average SAT scores, the correlation is not obvious in other schools
+
+# In[30]:
+
+
+his_schools=combined[combined['hispanic_per']>95]
+his_schools
+
+
+# In[29]:
+
+
+leshis_schools=combined[(combined['hispanic_per']<10) & (combined['sat_score']>1800)]
+leshis_schools
+
+
+# In[31]:
+
+
+combined.corr()['sat_score'][['female_per','male_per']].plot.bar()
+
+
+# In[32]:
+
+
+combined.plot.scatter('female_per','sat_score')
+
+
+# There is no significant correlation showing that female percentage has any relation to sat socres
+
+# In[33]:
+
+
+combined[(combined['female_per']>60) & (combined['sat_score']>1700)]['SCHOOL NAME']
+
+
+# In[34]:
+
+
+combined['ap_per']=combined['AP Test Takers ']/combined['total_enrollment']
+
+
+# In[36]:
+
+
+combined.plot.scatter('ap_per','sat_score')
+
